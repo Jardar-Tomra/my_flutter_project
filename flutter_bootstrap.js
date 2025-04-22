a54eb22682e38212499cee667a6ec4ef24ca6e91
Jardar-Tomra <Jardar-Tomra@users.noreply.github.com>
2025-04-22 16:13:56 +0000

    deploy: 5727408913f553b477daf885a5736586450025a5

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3097428230"
+    serviceWorkerVersion: "3500220779"
   }
 });
